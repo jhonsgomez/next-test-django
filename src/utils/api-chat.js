63c75b72793772a83de3d3c2800b1c3d70ff2050
jhonsgomez <jhonsebastiangomezsierra@gmail.com>
2024-11-08 16:26:19 -0500

    first commit

--- src/utils/api-chat.js
+++ src/utils/api-chat.js
@@ -3,14 +3,12 @@ import axios from 'axios';
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getMessages() {
-
-    console.log("API_URL:", API_URL)
-    // try {
-    //     const response = await axios.get(API_URL + "messages/");
-    //     return response.data
-    // } catch (error) {
-    //     console.log("Error obteniendo mensajes", error);
-    // }
+    try {
+        const response = await axios.get(API_URL + "messages/");
+        return response.data
+    } catch (error) {
+        console.log("Error obteniendo mensajes", error);
+    }
 }
 
 export async function createMessage(username, content) {
